7d50d3bd27eaaa2a84bb28091bdebe6c691bd79a
gkwh <gkwh@clare.(none)>
2015-02-03 13:09:33 -0600

    Sample Hello world version Juan Test

---
+++ hw_00_id_00.cpp
@@ -0,0 +1,14 @@
+Author: Juan Test
+Data:   Today
+Rev:    1.0
+
+/**
+  The canonical first sample for all langauajes programming
+  say: Hello World!!!
+*/
+
+int main()
+{
+  std::cout<<"Hello World"<<std::endl;
+  return 0;
+}
